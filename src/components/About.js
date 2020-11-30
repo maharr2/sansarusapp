8d63ce99568c7ddccc470c1761f3b63eb52a5df6
Rashmi <rashmimaharr2@gmail.com>
2020-11-29 20:59:19 -0800

    rel="noopener noreferrer" added on each file

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import '../App.css';
 import sansarusLogo from '../images/sansarus-logo.png';
-import reactLogo from '../images/react-logo.png';
 
 class About extends Component {
   render() {
@@ -9,7 +8,7 @@ class About extends Component {
       <div className="about">
         <h2> Sansarus is a consulting company, committed to connecting highly proficient technology professionals and clients in various industries.</h2>
   <p className="plus">..</p>
-<a target="_blank" href="https://www.sansarus.com">
+<a target="_blank"  rel="noopener noreferrer" href="https://www.sansarus.com">
           <img src={sansarusLogo} className="sansarus-logo" alt="sansarus-logo" />
         </a>
 <h4>Sansarus is dedicated to deliver business value to our clients.</h4>
